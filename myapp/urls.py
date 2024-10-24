3085ef53dd2ed441336aecbe683f9b8239340ef9
shrmsarthak <shrmsarthak2661@gmail.com>
2024-10-24 14:53:53 +0530

    test new

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('index_two', views.index_two, name='index_two'),
+    path('index_two/', views.index_two, name='index_two'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,189 +1,125 @@
-from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
-import pickle
+from django.urls import reverse
+from urllib.parse import urlencode
 
-from reportlab.pdfgen import canvas
-import pathlib
-from pathlib import Path
+# Global variable to store items (since you're avoiding session/pickle)
+items_list = []
 
-from random import *
-from datetime import datetime
-
-import os
-from django.conf import settings
-from django.http import HttpResponse, Http404 
-
-from django.http import FileResponse
-
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+# View to handle the first form submission
 def index(request):
-
     if request.method == "POST":
+        # Collect form data
         name = request.POST.get('name')
         address = request.POST.get("address")
         province = request.POST.get("province")
         country = request.POST.get("country")
         postalcode = request.POST.get("postalcode")
 
-        # request.session["name"] = name
-        # request.session["address"] = address
-        # request.session["province"] = province
-        # request.session["country"] = country
-        # request.session["postalcode"] = postalcode
+        # Dictionary to store the form data
+        user_data = {
+            "name": name,
+            "address": address,
+            "province": province,
+            "country": country,
+            "postalcode": postalcode
+        }
 
-        pickle_out = open("invoice.pickle","wb")
-        pickle.dump({}, pickle_out)
-        pickle_out.close()
+        # Build the query string and construct the full URL
+        query_string = urlencode(user_data)
+        url = reverse('index_two') + '?' + query_string
 
-        return redirect('index_two')
+        # Redirect to index_two with the query parameters
+        return redirect(url)
 
     return render(request, "index.html")
 
-def index_two(request):
 
-    items_dict = {}
+# View to handle item addition and invoice generation
+def index_two(request):
+    global items_list  # Use global list to store items
+
+    # Retrieve user data from query parameters
+    user_data = {
+        "name": request.GET.get("name"),
+        "address": request.GET.get("address"),
+        "province": request.GET.get("province"),
+        "country": request.GET.get("country"),
+        "postalcode": request.GET.get("postalcode")
+    }
 
     if 'additem' in request.POST:
-
-        try:
-            pickle_in = open("invoice.pickle","rb")
-            items_dict = pickle.load(pickle_in)
-        except:
-            pass
-
         description = request.POST.get("description")
         quantityorarea = request.POST.get("quantityorarea")
         unitprice = request.POST.get("unitprice")
 
-        if(len(description) != 0 and len(quantityorarea) !=0 and len(unitprice) != 0):
-            # request.session["description"] = description
-            # request.session["quantityorarea"] = quantityorarea
-            # request.session["unitprice"] = unitprice
-
-            items_dict["item"+str(len(items_dict))] = {'description':description,
-            'quantityorarea':quantityorarea,
-            'unitprice':unitprice}
-
-            pickle_out = open("invoice.pickle","wb")
-            pickle.dump(items_dict, pickle_out)
-            pickle_out.close()
-
-            print(items_dict)
-
+        if description and quantityorarea and unitprice:
+            # Append item to the global list
+            items_list.append({
+                'description': description,
+                'quantityorarea': quantityorarea,
+                'unitprice': unitprice
+            })
         else:
-            return render(request, 'index_two.html', {"mymessage": "Please enter data in the given field.","Flag":"True"})
+            return render(request, 'index_two.html', {
+                "mymessage": "Please enter data in the given fields.",
+                "Flag": "True",
+                'user_data': user_data,
+                'items_list': items_list
+            })
 
     elif 'generateinvoice' in request.POST:
+        from reportlab.pdfgen import canvas
+        from datetime import datetime
+        from random import randint
+        import os
+        from django.conf import settings
+        from django.http import FileResponse
 
+        # Retrieve user info
+        name = user_data["name"]
+        address = user_data["address"]
+        province = user_data["province"]
+        country = user_data["country"]
+        postalcode = user_data["postalcode"]
+        postalcode = postalcode[:3] + " " + postalcode[3:]
 
-        pickle_in = open("invoice.pickle","rb")
-        items_dict = pickle.load(pickle_in)
-
-        # name = request.session["name"]
-        # address = request.session["address"]
-        # province = request.session["province"]
-        # country = request.session["country"]
-        # postalcode = request.session["postalcode"]
-        # postalcode = postalcode[:3]+" "+postalcode[3:]
-
+        # Create PDF invoice
+        BASE_DIR = settings.BASE_DIR
         outfilename = os.path.join(BASE_DIR, 'myapp/static/myapp/final_invoice.pdf')
-
         can = canvas.Canvas(outfilename)
 
-        #head
-        file_name = os.path.join(BASE_DIR, 'myapp/static/myapp/header.png')
-
-        #billed to
-        can.drawImage(file_name, 0, 570, width=570, preserveAspectRatio=True, mask='auto')
-        can.setFont("Helvetica", 10)
-
-        can.drawString(25, 720, "Date: "+datetime.today().strftime('%Y-%m-%d'))
-        can.drawString(25, 710, "Invoice No. "+str(randint(1, 100000)))
+        # Header and basic details
+        can.drawString(25, 720, "Date: " + datetime.today().strftime('%Y-%m-%d'))
+        can.drawString(25, 710, "Invoice No. " + str(randint(1, 100000)))
 
         can.drawString(25, 680, "BILLED TO")
         can.drawString(25, 660, name)
-        can.drawString(25, 650, address+", "+province)
-        can.drawString(25, 640, country+" "+postalcode)
+        can.drawString(25, 650, address + ", " + province)
+        can.drawString(25, 640, country + " " + postalcode)
 
-        can.line(570, 610, 25, 610)
-        #owner name
-        can.setFont("Helvetica", 10)
+        # Invoice items
+        base = 595
+        current_base = base
 
-        can.drawString(345, 700, "Amandeep Singh")
-        can.drawString(345, 680, "AR Home Renovation & Construction Inc. C/O")
-        can.drawString(345, 670, "411 Popular Avenue, Summerside")
-        can.drawString(345, 660, "Canada C1N 2B9")
-        
+        for item in items_list:
+            description = item['description']
+            quantity = item['quantityorarea']
+            price = item['unitprice']
+            total = int(quantity) * int(price)
 
-        can.drawString(25, 595, "Description")
-        can.drawString(225, 595, "Quantity or Area")
-        can.drawString(400, 595, "Price")
-        can.drawString(520, 595, "Total")
+            can.drawString(25, current_base - 25, description)
+            can.drawString(260, current_base - 25, quantity)
+            can.drawString(405, current_base - 25, "$" + price)
+            can.drawString(525, current_base - 25, "$" + str(total))
 
-        can.line(570, 585, 25, 585)
+            current_base -= 25
 
-        description_array = []
-        quantityorarea_array = []
-        unitprice_array = []
-        total_array = []
-
-        for i in items_dict:
-            description_array.append(items_dict.get(i)["description"])
-            quantityorarea_array.append(items_dict.get(i)["quantityorarea"])
-            unitprice_array.append(items_dict.get(i)["unitprice"])
-            total_array.append(str(int(items_dict.get(i)["quantityorarea"]) * int(items_dict.get(i)["unitprice"])))
-
-        base  = 595
-        current_base = 0
-
-        for i in range(len(description_array)):
-            can.drawString(25, base-(i+1)*25, description_array[i])
-            can.drawString(260, base-(i+1)*25, quantityorarea_array[i])
-            can.drawString(405, base-(i+1)*25, "$"+unitprice_array[i])
-            can.drawString(525, base-(i+1)*25, "$"+total_array[i])
-
-            if i == len(description_array)-1:
-                current_base = base-(i+1)*25
-
-        can.line(570, current_base-12, 25, current_base-12)
-
-        #sub total
-        subtotal = 0
-        for i in total_array:
-            subtotal += int(i)
-
-        can.drawString(405, current_base-30, "Sub Total")
-        can.drawString(525, current_base-30, "$"+str(subtotal))
-
-        #Total after tax
-        can.drawString(405, current_base-50, "Tax (15%)")
-        can.drawString(525, current_base-50, "$"+str(subtotal*.15))
-
-        can.line(570, current_base-60, 400, current_base-60)
-
-        can.drawString(405, current_base-80, "Total")
-        can.drawString(525, current_base-80, "$"+str(subtotal + (subtotal*.15)))
-
-        can.drawString(25, 175, "CONTACT INFORMATION")
-        can.drawString(25, 150, "Phone : 647-622-4449")
-        can.drawString(25, 140, "Email: Arhomerenovation1@gmail.com")
-        can.drawString(25, 130, "Website: https://arhomerenovation.ca/")
-
-        can.drawString(450, 130, "HST No. 732134002RT001")
-
-        footer_filename = os.path.join(BASE_DIR, 'myapp/static/myapp/footer.png')
-
-        can.drawImage(footer_filename, 0, -130, width=570, preserveAspectRatio=True, mask='auto')
-
-        can.showPage()
+        # Finalize PDF and return it as a file response
         can.save()
-
         return FileResponse(open(outfilename, 'rb'), as_attachment=True)
-        # return render(request, 'index_two.html', {"mymessage": "Invoice Generated.","Flag":"True"})
 
-
-    return render(request, "index_two.html")
-
-
+    # Render the page with the form and items
+    return render(request, "index_two.html", {
+        'user_data': user_data,
+        'items_list': items_list
+    })
